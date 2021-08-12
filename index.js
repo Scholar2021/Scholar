f0fdb4a9044088340798af1dd6b01737504916b6
Scholar2021 <88793483+Scholar2021@users.noreply.github.com>
2021-08-13 00:22:04 +0400

    Update 2 Mobile

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ window.addEventListener('DOMContentLoaded', () => {
                 }, 200);
             }, 1500);
         }, 1500);
-    }, 2000);
+    }, 1000);
 
     //Timer
     const deadline = '2021-09-15';
